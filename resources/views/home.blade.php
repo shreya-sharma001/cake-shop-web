09b5fba5fa9e6e91cbabb8b177e8d61a24c93b00
shreya-sharma001 <shreya.sharma09871@gmail.com>
2025-06-18 11:28:00 +0530

    changes in home.blade file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
                     <div class="row d-flex justify-content-center">
                         <div class="col-lg-8">
                             <div class="hero__text">
-                                <h2>Making your life sweeter one bite at a time!</h2>
+                                <h2>Making your life sweeter one bite at a time.</h2>
                                 <a href="#" class="primary-btn">Our cakes</a>
                             </div>
                         </div>
